53d7ae6f0853151fc6d60c165bc975b34ffb6468
mayor <jason@optionzero.co>
2026-02-28 20:57:20 -0800

    WIP: Cloudflare Workers deployment config (incomplete)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  serverRuntimeConfig: {
+    // Only available on the server side
+    databaseUrl: process.env.DATABASE_URL,
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
+  },
 }
 
 module.exports = nextConfig
